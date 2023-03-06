b885de918fca4f0e2e86219f7671ad318b8f18ce
plam3nk <114010915+plam3nk@users.noreply.github.com>
2023-03-06 23:41:34 +0200

    Exercise

---
+++ Exercise Inheritance/Need For Speed/project/vehicle.py
@@ -0,0 +1,11 @@
+class Vehicle:
+    DEFAULT_FUEL_CONSUMPTION = 1.25
+
+    def __init__(self, fuel: float, horse_power: int):
+        self.fuel_consumption = self.DEFAULT_FUEL_CONSUMPTION
+        self.fuel = fuel
+        self.horse_power = horse_power
+
+    def drive(self, kilometres: float) -> None:
+        if self.fuel - kilometres * self.fuel_consumption >= 0:
+            self.fuel -= kilometres * self.fuel_consumption
